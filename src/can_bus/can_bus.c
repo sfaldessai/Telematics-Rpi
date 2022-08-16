8c4c0cd4622b49a5b2aadd62130f6176b68bb289
user <user@users-MacBook-Air.local>
2022-08-16 20:08:48 +0530

    added threads for fetcing supported pid data and speed pid data from can

--- src/can_bus/can_bus.c
+++ src/can_bus/can_bus.c
@@ -6,61 +6,25 @@
  */
 
 #include <stdio.h>
+#include <pthread.h>
 #include "can_bus.h"
 
-void read_can_id_number(void *arg)
+/* mutex to lock cloud_data struct for wirte */
+pthread_mutex_t can_bus_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+char *read_can_id_number(uart_device_struct *can_device)
 {
-    struct can_data_Struct v_in;
-    unsigned int l_data;
-    unsigned int l_pid;
-    if (l_pid == 0x02)
-    {
-    }
+    char read_data[17];
+
+    uart_write(&can_device, "0x02\r\n");
+
+    sleep(1);
+
+    uart_reads_chunk(&client_controller_device, &read_data, MAX_READ_SIZE);
 }
 
 // for every 1000ms read vehicle speed data
 void *read_can_speed_pid(void *arg)
-{
-    struct can_data_Struct v_speed;
-    unsigned int l_data;
-    unsigned int l_pid;
-    while (1)
-    {
-        l_pid = 0;
-        l_data = 0;
-        sleep(1);
-        if (l_pid == 0x0d)
-        {
-        }
-    }
-}
-
-// for every 30ms read PID 0x00
-void *read_can_supported_pid(void *arg)
-{
-    struct can_data_Struct poll_data;
-    unsigned int l_data;
-    unsigned int l_pid;
-    while (1)
-    {
-        l_pid = 0;
-        l_data = 0;
-        sleep(30);
-        if (l_pid == 0x00)
-        {
-        }
-    }
-}
-
-// for every 30ms read PID 0x00
-void *read_canAble(void *arg)
-{
-    /* TODO */
-    printf("%p", arg);
-    return 0;
-}
-
-void *read_from_can(void *arg)
 {
     char *read_data = NULL;
 
@@ -69,11 +33,65 @@ void *read_from_can(void *arg)
     struct cloud_data_struct *cloud_data = args->cloud_data;
     struct can_data_Struct can_data;
 
-    read_can_id_number(&can_device);
-
-    do
+    while (1)
     {
-        // read_can_id_number(&can_data);
-    } while (1);
-    uart_stop(&can_device);
+        pthread_mutex_lock(&cloud_data_mutex);
+
+        uart_write(&can_device, "0x0d\r\n");
+
+        sleep(1);
+
+        uart_reads_chunk(&client_controller_device, &read_data, MAX_READ_SIZE);
+
+        can_data.speed = (uint8_t)atoi(read_data);
+
+        uint8_t speed;
+
+        pthread_mutex_unlock(&cloud_data_mutex);
+    }
+}
+
+// for every 30ms read PID 0x00
+void *read_can_supported_pid(void *arg)
+{
+    char *read_data = NULL;
+
+    struct arg_struct *args = (struct arg_struct *)arg;
+    struct uart_device_struct can_device = args->uart_device;
+    struct cloud_data_struct *cloud_data = args->cloud_data;
+    struct can_data_Struct can_data;
+
+    while (1)
+    {
+        pthread_mutex_lock(&cloud_data_mutex);
+
+        uart_write(&can_device, "0x00\r\n");
+
+        sleep(30);
+
+        uart_reads_chunk(&client_controller_device, &read_data, MAX_READ_SIZE);
+
+        can_data.supported_pids = (uint32_t)atoi(read_data);
+
+        pthread_mutex_unlock(&cloud_data_mutex);
+    }
+}
+
+void *read_from_can(void *arg)
+{
+    char *read_data = NULL;
+    pthread_t read_can_supported_thread, read_can_speed_thread;
+
+    struct arg_struct *args = (struct arg_struct *)arg;
+    struct uart_device_struct can_device = args->uart_device;
+    struct cloud_data_struct *cloud_data = args->cloud_data;
+    struct can_data_Struct can_data;
+
+    can_data.vin = (uint8_t *)read_can_id_number(&can_device);
+
+    pthread_create(&read_can_supported_thread, NULL, &read_can_supported_pid, &arg);
+    pthread_create(&read_can_speed_thread, NULL, &read_can_speed_pid, &arg);
+
+    pthread_join(read_can_supported_thread, NULL);
+    pthread_join(read_can_speed_thread, NULL);
 }
