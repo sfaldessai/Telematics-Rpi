cfff2a5ea3da1f824ef3cfc4e88d1f6c3f98ec78
user <user@users-MacBook-Air.local>
2022-09-13 12:15:56 +0530

    Resolved sqlite3 db insertion issue and optimised db inserion operation code

--- src/database/db_handler.h
+++ src/database/db_handler.h
@@ -12,6 +12,34 @@
 
 #define DEBUG
 
+/* DB columns names */
+#define LATITUDE "Latitude"
+#define LATITUDE_SIGN "LatSign"
+#define LONGITUDE "Longitude"
+#define LONGITUDE_SIGN "LongSign"
+#define PDOP "PDOP"
+#define HDOP "HDOP"
+#define VDOP "VDOP"
+#define SERIAL "Serial"
+#define VIN "VIN"
+#define SPEED "Speed"
+#define SUPPORTED_PIDs "Supported_Pids"
+#define DISTANCE_TRAVLLED "Dist_Travelled"
+#define IDLE_TIME "Idle_time"
+#define VEHICLE_IN_SERVICE "Veh_in_Service"
+#define MOTION "Motion"
+#define VOLTAGE "Voltage"
+#define PTO "PTO"
+#define ACC_X "AccX"
+#define ACC_Y "AccY"
+#define ACC_Z "AccZ"
+#define RPM "RPM"
+#define TEMPERATURE "Temperature"
+
+#define TELEMATICS_DB_PATH "/usr/sbin/telematic.db"
+#define SQL_CREATE_TABLE "CREATE TABLE IF NOT EXISTS TELEMATICS(ID INTEGER PRIMARY KEY AUTOINCREMENT, creation_time TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, Latitude FLOAT, LatSign Text, Longitude FLOAT, LongSign Text, PDOP FLOAT, HDOP FLOAT, VDOP FLOAT,Serial TEXT, VIN TEXT, Speed INT, Supported_Pids TEXT, Dist_Travelled FLOAT, Idle_time FLOAT, Veh_in_Service FLOAT,Motion INT, Voltage FLOAT, PTO INT,AccX FLOAT, AccY FLOAT, AccZ FLOAT,RPM INT, Temperature FLOAT);"
+#define DB_QUERY "INSERT INTO TELEMATICS (Latitude,LatSign,Longitude,LongSign,PDOP,HDOP,VDOP,Serial,VIN,Speed,Supported_Pids,Dist_Travelled,Idle_time,Veh_in_Service,Motion,Voltage,PTO,AccX,AccY,AccZ,RPM,Temperature) VALUES (%f,'%c',%f,'%c',%f,%f,%f,'%s','%s',%d, '%s','%f','%f','%f',%d,%f,%d,%f,%f,%f,%d,%f)"
+
 int initialize_db(void);
 
 int insert_telematics_data(struct cloud_data_struct* inCloud_data);
