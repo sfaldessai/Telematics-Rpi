659ec8e67dad2c560b82f4530cfec52144d7d9cb
user <user@users-MacBook-Air.local>
2022-10-04 16:28:20 +0530

    added json object

--- src/cloud_server/cloud_write.c
+++ src/cloud_server/cloud_write.c
@@ -13,7 +13,8 @@
 #include "cloud_server.h"
 #include "../logger/logger.h"
 #include "../database/db_handler.h"
-
+#include "../utils/c_json/cJSON.h"
+#include "mqtt_demo_mutual_auth.h"
 #define HOURS 3600
 #define RPM_OFFSET 0
 #define SPEED_THRESHOLD 0
@@ -21,6 +22,45 @@
 
 bool idle_timer_started = false;
 time_t begin, end;
+
+char *jsonObj(struct cloud_data_struct *cloud_data)
+{
+    cJSON *cjson_vehicle = NULL;
+    cJSON *cjson_telematic = NULL;
+    cJSON *cjson_can = NULL;
+    cJSON *cjson_location = NULL;
+    char *str = NULL;
+
+    cjson_telematic = cJSON_CreateObject();
+    cjson_can = cJSON_CreateObject();
+    cjson_vehicle = cJSON_CreateObject();
+
+    // change these values in json file
+    cJSON_AddStringToObject(cjson_can, "serial", (char *)cloud_data->mac_address);
+    cJSON_AddStringToObject(cjson_can, "from", "SHREE");
+    cJSON_AddStringToObject(cjson_can, "vin", (char *)cloud_data->can_data.vin);
+    cJSON_AddNumberToObject(cjson_can, "speed", cloud_data->can_data.speed);
+    // cJSON_AddNumberToObject(cjson_can, "distance_travelled", distanceTravelled);
+    cJSON_AddNumberToObject(cjson_can, "idle_time", cloud_data->idle_time_secs);
+    // cJSON_AddNumberToObject(cjson_can,"veh_in_service", inService);
+    cJSON_AddItemToObject(cjson_telematic, "can", cjson_can);
+
+    cjson_location = cJSON_CreateObject();
+    cJSON_AddNumberToObject(cjson_location, "lat", cloud_data->gps_data.longitude);
+    cJSON_AddNumberToObject(cjson_location, "long", cloud_data->gps_data.longitude);
+    cJSON_AddNumberToObject(cjson_location, "dop", cloud_data->gps_data.hdop);
+    cJSON_AddItemToObject(cjson_telematic, "location", cjson_location);
+
+    cJSON_AddNumberToObject(cjson_telematic, "motion", cloud_data->client_controller_data.motion);
+    cJSON_AddNumberToObject(cjson_telematic, "pto", cloud_data->client_controller_data.pto);
+    cJSON_AddNumberToObject(cjson_telematic, "battery", cloud_data->client_controller_data.voltage);
+
+    cJSON_AddItemToObject(cjson_vehicle, "telematic", cjson_telematic);
+
+    /* Prints all the data of the JSON object (the whole list) */
+    str = cJSON_Print(cjson_vehicle);
+    return str;
+}
 
 void display_cloud_struct_data_logger(struct cloud_data_struct *cloud_data)
 {
@@ -47,13 +87,15 @@ void *write_to_cloud(void *arg)
 
     /* Initializing logger */
     logger_setup();
-
     int rc = initialize_db();
     while (1)
     {
         if (cloud_data != NULL)
         {
             display_cloud_struct_data_logger(cloud_data);
+
+            send_data = jsonObj(cloud_data);
+            // printf("\n TEST DATA======%s\n", send_data);
 
             calculate_idle_time(cloud_data);
 
@@ -79,7 +121,7 @@ void *write_to_cloud(void *arg)
 void calculate_idle_time(struct cloud_data_struct *cloud_data)
 {
     /* Start the Idle time when the speed is 0 and RPM is greater than some threshold when Ignition is ON */
-    if ((cloud_data->can_data.speed == SPEED_THRESHOLD) &&
+    if ((cloud_data->can_data.speed == SPEED_THRESHOLD && cloud_data->gps_data.speed == SPEED_THRESHOLD) &&
         (cloud_data->can_data.rpm >= RPM_OFFSET))
     {
         if (idle_timer_started == false)
@@ -139,4 +181,25 @@ void initialize_cloud_data(struct cloud_data_struct *cloud_data)
     uint8_t idle_time_db_value[COLUMN_VALUE_MAX_LEN];
     get_single_column_value(IDLE_TIME, SORT_BY_DESC, idle_time_db_value);
     cloud_data->idle_time_secs = (uint64_t)atoi((char *)idle_time_db_value);
+}
+
+/*
+ * Name : gps_error_codes
+ * Descriptoin: The gps_error_codes function is for updating erro codes for gps struct member
+ * Input parameters: struct cloud_data_struct * : clout struct to update gps data member
+ *                   int error_code : error code to update
+ * Output parameters: void
+ */
+void gps_error_codes(struct cloud_data_struct *cloud_data, int error_code)
+{
+    struct gps_data_struct gps_data;
+
+    gps_data.hdop = error_code;
+    gps_data.vdop = error_code;
+    gps_data.pdop = error_code;
+    gps_data.latitude = error_code;
+    gps_data.longitude = error_code;
+    gps_data.speed = error_code;
+
+    cloud_data->gps_data = gps_data;
 }
