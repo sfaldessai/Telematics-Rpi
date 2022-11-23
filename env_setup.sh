f6e232b3ec9ee401926f0cb6111e06ff48a50aef
GirishKulkarniGK <girkulkarni@deloitte.com>
2022-11-23 15:29:36 +0530

    fixed shell scripts

--- env_setup.sh
+++ env_setup.sh
@@ -1,11 +1,11 @@
-REQUIRED_PKG="libsqlite3-dev gcc cmake libbluettoth3-dev git build-essential"
-for i in $REQUIRED_PKG; do
-	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $i|grep "install ok installed")
-	echo Checking for $i:$PKG_OK
-	if [""="$PKG_OK"];then
-		echo"No $i.Setting up $i."
+REQUIRED_PKG="libsqlite3-dev gcc cmake libbluetooth3-dev git build-essential"
+for i in $REQUIRED_PKG;do 
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $i)
+	echo Checkng for $i:$PKG_OK
+	if [ "" = "$PKG_OK" ]; then
+		echo "NO $i,Setting up $i."
 		sudo apt-get --yes install $i
-	fi	
+	fi
 done
 sudo make main
 sudo ./telematic -m 6 -f 1
