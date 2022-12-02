473aa212ee4e4514b4ca37ec35b984388b6baea3
nvenkatasrikanth <115691008+nvenkatasrikanth@users.noreply.github.com>
2022-12-02 13:24:35 +0530

    Renamed can_bus.cpp~ to can_bus_test.cpp

---
+++ tests/can_bus/can_bus_test.cpp
@@ -0,0 +1,151 @@
+#include "CppUTest/CommandLineTestRunner.h"
+#include "CppUTestExt/MockSupportPlugin.h"
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+#include <stdbool.h>
+
+extern "C"
+{
+#include "./../src/can_bus/can_bus.h"
+#include "./../src/cloud_server/cloud_server.h"
+}
+
+TEST_GROUP(CanBusTestGroup)
+{
+    void setup(){};
+    void teardown(){};
+};
+
+TEST(CanBusTestGroup, validateVinValidTest)
+{
+    /*arrange*/
+    char *vin = (char *)"5YJSA1DG9DFP14705";
+
+    /*act*/
+    bool result = validate_vin(vin);
+
+    /*assert*/
+    CHECK_EQUAL(1, result);
+}
+
+TEST(CanBusTestGroup, validateVinInValidTest)
+{
+    /*arrange*/
+    char *vin = (char *)"5YJSA3DG9HFP14703";
+
+    /*act*/
+    bool result = validate_vin(vin);
+
+    /*assert*/
+    CHECK_EQUAL(0, result);
+}
+
+TEST(CanBusTestGroup, validateVinLenInValidTest)
+{
+    /*arrange*/
+    char *vin = (char *)"5YJSA3DG9";
+
+    /*act*/
+    bool result = validate_vin(vin);
+
+    /*assert*/
+    CHECK_EQUAL(0, result);
+}
+
+TEST(CanBusTestGroup, getManufacturerDetailValidTest)
+{
+    /*arrange*/
+    uint8_t *wmi = (uint8_t *)"5YJ";
+
+    /*act*/
+    char *vehicle_detail = get_manufacturer_detail(wmi);
+
+    /*assert*/
+    STRCMP_EQUAL("Tesla Motors", vehicle_detail);
+}
+
+TEST(CanBusTestGroup, getManufacturerDetailInValidTest)
+{
+    /*arrange*/
+    uint8_t *wmi = (uint8_t *)"5YO";
+
+    /*act*/
+    char *vehicle_detail = get_manufacturer_detail(wmi);
+
+    /*assert*/
+    STRCMP_EQUAL(NULL, vehicle_detail);
+}
+
+TEST(CanBusTestGroup, readCanTemperaturePidTest)
+{
+    /*arrange*/
+    struct cloud_data_struct cloud_data; // = (struct cloud_data_struct)malloc(sizeof(struct cloud_data_struct));
+     cloud_data.can_data.mode = 0;
+    pthread_t read_can_supported_thread, read_can_speed_thread, read_can_vin_thread, read_can_rpm_thread, read_can_temperature_thread;
+
+    /*act*/
+    read_from_can(&cloud_data, &read_can_supported_thread, &read_can_speed_thread, &read_can_vin_thread, &read_can_rpm_thread, &read_can_temperature_thread);
+
+    /*assert*/
+    CHECK(cloud_data.can_data.speed > 0);
+}
+TEST(CanBusTestGroup, read_can_supported_pid)
+{
+    /*arrange*/
+     struct cloud_data_struct cloud_data;
+     //struct can_frame = (struct )"0";
+
+
+
+   /*act*/
+   read_can_supported_pid(&cloud_data);
+
+
+
+   /*assert*/
+    //CHECK_EQUAL(1, supportedframe);
+    CHECK(cloud_data.can_data.supported_pids[0] >= 0);
+}
+
+
+
+TEST(CanBusTestGroup, read_can_speed_pid)
+{
+    /*arrange*/
+    struct cloud_data_struct cloud_data;
+
+
+
+   /*act*/
+    read_can_supported_pid(&cloud_data);
+
+
+
+   /*assert*/
+    CHECK(cloud_data.can_data.speed >= 0);
+}
+
+
+
+TEST(CanBusTestGroup, read_can_rpm_pid)
+{
+    /*arrange*/
+    struct cloud_data_struct cloud_data;
+   /*act*/
+    read_can_supported_pid(&cloud_data);
+   /*assert*/
+    CHECK(cloud_data.can_data.rpm >= 0 );
+}
+
+TEST(CanBusTestGroup, read_can_id_number)
+{
+	/*arrange*/
+	struct cloud_data_struct cloud_data;
+
+	/*act*/
+	read_can_supported_pid(&cloud_data);
+
+	/*assert*/
+/*	CHECK(cloud_data.can_data.vin >= 0);*/
+}
+
